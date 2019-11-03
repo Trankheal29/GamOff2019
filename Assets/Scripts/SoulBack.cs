fba13cd959ce61973fd4c28480f0892d7c2fda41
Trankheal29 <trankheal29@gmail.com>
2019-11-03 14:22:27 +0100

    TPsystem

--- Assets/Scripts/SoulBack.cs
+++ Assets/Scripts/SoulBack.cs
@@ -6,17 +6,50 @@ public class SoulBack : MonoBehaviour
 {
     
     public GameObject soul;
+    GameObject playerInScene;
+    public GameObject playerRespawn;
+    Rigidbody2D rb;
     // Update is called once per frame
     void Update()
     {
+        GameObject playerInScene = GameObject.Find("Player");
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
-            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
-            GameObject.Find("Player").GetComponent<SoulCast>().enabled = true;
-            Destroy(soul);
-            
+           
+            if (playerInScene) {
+
+                rb = playerInScene.GetComponent<Rigidbody2D>();
+                rb.velocity = new Vector2(0, 0);
+                playerInScene.transform.position = soul.transform.position;
+                playerInScene.GetComponent<PlayerMovement>().enabled = true;
+                playerInScene.GetComponent<SoulCast>().enabled = true;
+                Destroy(soul,0.1f);
+
+            }
+            else
+            {
+                playerRespawn.transform.position = soul.transform.position;
+                Instantiate(playerRespawn);
+                Destroy(soul);
+              
+            }
+
+
         }
 
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (playerInScene)
+            {
+            playerInScene.GetComponent<PlayerMovement>().enabled = true;
+            playerInScene.GetComponent<SoulCast>().enabled = true;
+            Destroy(soul);
+            }
+            else
+            {
+                Debug.Log("No body available");
+            }
+        }
     }
 }
