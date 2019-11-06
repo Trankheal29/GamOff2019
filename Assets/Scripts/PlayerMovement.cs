07a461e0d854a30d38a68b279ae9b1c878e91034
Trankheal29 <trankheal29@gmail.com>
2019-11-06 10:10:53 +0100

    game over

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-   
-
-
+    public float climbSpeed;
     float moveInput;
+    float climbInput;
+    public float wallCheckDistance;
+
+    private Vector2 ledgePosBot;
+
+
     Rigidbody2D rb;
+
+
+    public Transform ledgeCheck;
+    public Transform wallCheck;
+
+    public LayerMask whatIsGround;
+
+    private bool isFacingRight = true;
+    private bool isTouchingWall;
+    private bool isTouchingLedge;
+    private bool canClimbLedge = false;
+    private bool ledgeDetected = false;
    
     
 
@@ -19,12 +35,52 @@ public class PlayerMovement : MonoBehaviour
        
     }
     // Update is called once per frame
-   
+
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        climbInput = Input.GetAxisRaw("Vertical");
+        CheckSurroundings();
+        DirectionCheck();
+    }
 
     void FixedUpdate()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed * Time.deltaTime, rb.velocity.y);
+        
+         rb.velocity = new Vector2(moveInput * speed * Time.deltaTime, rb.velocity.y);
+        if(ledgeDetected == true && climbInput > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, climbInput * climbSpeed * Time.deltaTime);
+            ledgeDetected = false;
+
+        }
         
     }
+
+    void DirectionCheck()
+    {
+        if(moveInput > 0 && !isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            transform.localScale = new Vector3(1f, 1f, 0);
+
+        }
+        else if(moveInput<0 && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            transform.localScale = new Vector3(-1f, 1f, 0);
+        }
+    }
+
+    private void CheckSurroundings()
+    {
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+        isTouchingLedge = Physics2D.Raycast(ledgeCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if(isTouchingWall && !isTouchingLedge && !ledgeDetected)
+        {
+            ledgeDetected = true;
+        }
+    }
+
 }
--- Assets/Scripts/SoulManagement.cs
+++ Assets/Scripts/SoulManagement.cs
@@ -6,19 +6,24 @@ public class SoulManagement : MonoBehaviour
 {
     GameObject soulInScene;
     GameObject playerInScene;
+    Rigidbody2D rb2D;
     // Update is called once per frame
+    
     void Update()
     {
         playerInScene = GameObject.FindGameObjectWithTag("Player");
         soulInScene = GameObject.FindGameObjectWithTag("Soul");
+        rb2D = playerInScene.GetComponent<Rigidbody2D>();
 
         if (soulInScene != null)
         {
+            rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
             playerInScene.GetComponent<PlayerMovement>().enabled = false;
             playerInScene.GetComponent<SoulCast>().enabled = false;
         }
         else
         {
+            
             playerInScene.GetComponent<PlayerMovement>().enabled = true;
             playerInScene.GetComponent<SoulCast>().enabled = true; 
         }
